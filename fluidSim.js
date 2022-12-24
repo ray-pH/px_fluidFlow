78dfe12c4740d331490657bcdfd5e103ba490dc8
ray-pH <rayhan.azizi9@gmail.com>
2022-12-24 17:29:00 +0700

    improve draw() performance

--- fluidSim.js
+++ fluidSim.js
@@ -19,7 +19,8 @@ class FluidSimulator {
         this.n_iter = n_iter;
         this.dt = dt;
 
-        var  n  = this.nx * this.ny;
+        this.n  = this.nx * this.ny; 
+        let  n  = this.n;
         this.Vx = new Float32Array(n);
         this.Vy = new Float32Array(n);
 
@@ -289,49 +290,6 @@ class FluidRenderer{
         }
     }
 
-    putColor(color, i, j){
-        var p = 4 * (j * this.temp_canvas.width + i);
-        this.temp_pixels[p+0] = color[0];
-        this.temp_pixels[p+1] = color[1];
-        this.temp_pixels[p+2] = color[2];
-        this.temp_pixels[p+3] = color[3];
-    }
-
-    // buffer : keep in buffer, not draw to canvas yet
-    drawDye(buffer = false){
-        var fl = this.fluidsim;
-        var nx = fl.nx;
-
-        let color = [255, 255, 255, 255];
-        for (var i = 0; i < fl.nx; i++) { for (var j = 0; j < fl.ny; j++) {
-            color[0] = 255 * fl.RDye[i + nx*j];
-            color[1] = 255 * fl.GDye[i + nx*j];
-            color[2] = 255 * fl.BDye[i + nx*j];
-            color[3] = 255;
-
-            this.putColor(color, i, j);
-        }}
-
-        if (buffer) return;
-        this.temp_ctx.putImageData(this.temp_img_data, 0, 0);
-        this.ctx.drawImage(this.temp_canvas, 0, 0);
-    }
-
-    drawObstacle(buffer = false){
-        var fl = this.fluidsim;
-        var nx = fl.nx;
-
-        // #9bb6e0
-        let obs_color = hex2rgb(0x9BB6E0); //obstacle
-        for (var i = 0; i < fl.nx; i++) { for (var j = 0; j < fl.ny; j++) {
-            if (fl.S[i + nx*j] == 0) this.putColor(obs_color, i, j);
-        }}
-
-        if (buffer) return;
-        this.temp_ctx.putImageData(this.temp_img_data, 0, 0);
-        this.ctx.drawImage(this.temp_canvas, 0, 0);
-    }
-
     // TODO : buffer draw
     drawPressure(){
         var fl = this.fluidsim;
@@ -356,17 +314,32 @@ class FluidRenderer{
         this.ctx.drawImage(this.temp_canvas, 0, 0);
     }
 
-
     // var draw_option {
     //     'dye'       : true,
     //     'obstacle'  : true,
     //     'streamline': true,
     // }
     draw(draw_option) {
-        this.temp_canvas.clearRect(0, 0, temp_canvas.width, temp_canvas.height);
+        var fl = this.fluidsim;
 
-        if (draw_option['dye'])      this.drawDye(true);
-        if (draw_option['obstacle']) this.drawObstacle(true);
+        let color     = [255, 255, 255, 255];
+        let obs_color = hex2rgb(0x9BB6E0); //obstacle #9bb6e0
+        for (let i = 0; i < fl.n; i++){
+
+            if (draw_option['dye']){
+                color[0] = 255 * fl.RDye[i];
+                color[1] = 255 * fl.GDye[i];
+                color[2] = 255 * fl.BDye[i];
+                color[3] = 255;
+            }
+            if (fl.S[i] == 0 && draw_option['obstacle']) color = obs_color.slice();
+
+            var p = 4*i;
+            this.temp_pixels[p+0] = color[0];
+            this.temp_pixels[p+1] = color[1];
+            this.temp_pixels[p+2] = color[2];
+            this.temp_pixels[p+3] = color[3];
+        }
 
         // put data into temp_canvas
         this.temp_ctx.putImageData(this.temp_img_data, 0, 0);
